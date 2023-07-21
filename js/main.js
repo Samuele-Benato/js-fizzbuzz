313834d43a2c7d83d0f79c46f922b305a55f867a
Samuele-Benato <samuele.benato@gmail.com>
2023-07-21 14:55:20 +0200

    feat : add start js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Scrivi un programma che stampi in console i numeri da 1 a 100,
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    // Per i numeri che sono sia multipli di 3 che di 5 stampi "FizzBuzz".
+  } else if (i % 3 == 0) {
+    // per i multipli di 3 stampi "Fizz" al posto del numero
+  } else if (i % 5 == 0) {
+    // per i multipli di 5 stampi "Buzz" al posto del numero
+  }
+}
